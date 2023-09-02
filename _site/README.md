e5d8d68b39ca6237600189d35cf4c646ea0b5c65
tqthanhcong <43869861+tqthanhcong@users.noreply.github.com>
2023-09-02 21:47:46 +0700

    update

---
+++ _site/README.md
@@ -0,0 +1,2 @@
+# [Source: Start Bootstrap - Clean Blog Jekyll](https://startbootstrap.com/themes/clean-blog-jekyll/) - Official Jekyll Version
+
---
+++ _site/sw.js
@@ -0,0 +1,73 @@
+const version = '20230902212447';
+const cacheName = `static::${version}`;
+
+const buildContentBlob = () => {
+  return ["/2020/01/31/man-must-explore.html","/2020/01/31/heartbeats.html","/2020/01/30/prophecy.html","/2020/01/29/exploration.html","/2020/01/28/dreams.html","/2020/01/27/dinosaurs.html","/about.html","/contact.html","/resume/cv_james.html","/posts/","/resume/","/","/assets/main.css","/resumemarkdown.html","/resume/srt-resume.html","/feed.xml","/posts/page2/","/sitemap.xml","/robots.txt","/assets/main.css.map","/assets/logos/logo_dino.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  ]
+}
+
+const updateStaticCache = () => {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(buildContentBlob());
+  });
+};
+
+const clearOldCache = () => {
+  return caches.keys().then(keys => {
+    // Remove caches whose name is no longer valid.
+    return Promise.all(
+      keys
+        .filter(key => {
+          return key !== cacheName;
+        })
+        .map(key => {
+          console.log(`Service Worker: removing cache ${key}`);
+          return caches.delete(key);
+        })
+    );
+  });
+};
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    updateStaticCache().then(() => {
+      console.log(`Service Worker: cache updated to version: ${cacheName}`);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clearOldCache());
+});
+
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Only deal with requests from the same domain.
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  // Always fetch non-GET requests from the network.
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Default url returned if page isn't cached
+  let offlineAsset = "/offline/";
+
+  if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+    // If url requested is an image and isn't cached, return default offline image
+    offlineAsset = "/assets/default-offline-image.png";
+  }
+
+  // For all urls request image from network, then fallback to cache, then fallback to offline page
+  event.respondWith(
+    fetch(request).catch(async () => {
+      return (await caches.match(request)) || caches.match(offlineAsset);
+    })
+  );
+  return;
+});
